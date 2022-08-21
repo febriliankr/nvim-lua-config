37b95ed4bbab8d590de325abf419c5d5966f18f0
Febrilian <febrilian.kr@gmail.com>
2022-08-21 21:43:09 +0700

    make nvim transparent

--- lua/config/interface/colorscheme.lua
+++ lua/config/interface/colorscheme.lua
@@ -1,3 +1,4 @@
 vim.g.tokyonight_italic_functions = 1
 vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer", "neo-tree" }
 vim.cmd("colorscheme tokyonight")
+vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
